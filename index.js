b2debfe2b14781e5eea99d34792fe08b8ef4ca16
Jin Wu <jinn.wuu@gmail.com>
2020-02-16 15:02:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (value) {
   if (value > 42) {
-    return value - 42
+    return value - 42;
   }
   else (value < 42) {
-    return 42 - value
+    return 42 - value;
   }
 }
 
